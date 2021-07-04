392910ac419934da548fb6bfd01988c194d88618
matthew-devonport <roarsintheeast@gmail.com>
2021-07-04 13:08:34 +1200

    updated testimonials

--- client/components/Testimonials.jsx
+++ client/components/Testimonials.jsx
@@ -9,37 +9,100 @@ class Testimonials extends React.Component {
 
     render() {
         return (
-            <React.Fragment> 
-                    <TestNav/>
-                    <div className="background">
-                <img src="./img/logo.png" alt="Main logo" id="mainLogo" className="centerImg"></img>
-                <img src="./img/testimonials.png" alt="Main logo" id="testLogo" className="centerImg"></img>
+            <React.Fragment>
+                <TestNav />
+                <div className="background">
+                    <img src="./img/logo.png" alt="Main logo" id="mainLogo" className="centerImg"></img>
+                    <img src="./img/testimonials.png" alt="Main logo" id="testLogo" className="centerImg"></img>
                 </div>
                 <div id="testArea">
                     <h1 className="testTitle">Here are what some of our clients say!</h1>
-                    <div className="testText">"Had our old hot water cylinder die on us recently. Flash gas and plumbing came out, 
-                       installed new cylinder but unfortunately the pipes under the house were not up to the task. 
-                       So an infinity gas water system was installed. Wow what a difference, pressure is out of this world.
-                       Big ups to Cam, was a nightmare job but he was equal to the task.<br /><br /> Highly recommend to anyone."<br/><br/> - <i>Andrew Gunn</i></div>
+                    <div className="testText">"Exceptional service and very professional. Highly recommend."<br /><br /> - <i>Tanja Lendowsky</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">"Just had Cam come out to fix our burst hot water cylinder, super friendly, fast and efficient, great communication, will definitely recommend to anyone!
+                     Thanks Cam :)"<br /><br /> - <i>Makayla June</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">"Fabulous communication and service, a very pleasant young man. He put in a water filter and fixed our loo.
+                     Very happy to recommend. AAA rating"<br /><br /> - <i>Loretta McGregor</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">"Campbell came to our house and replaced our water cylinder after we were having a lot of trouble with it.
+                    He also gave us great advice with a blockage that was happening in the sink as well which help solved our issue.
+                     Professional, punctual and takes pride in getting the job done right."<br /><br /> - <i>Matthew Devonport</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">Positive: Professionalism, Punctuality, Quality, Responsiveness &amp; Value<br /><br />
+                        "Cam is a good  bloke and  does  a good tidy job will  use him again."<br /><br /> - <i>David Brown</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">"Superb service. Would recommend to all and should I require further work, I'll be booking again."<br /><br /> - <i>Dave Richardson</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">Positive: Professionalism, Quality &amp; Responsiveness<br /><br />
+                        "Cam gives great advice and easily explained. Highly recommended"<br /><br /> - <i>Steve Healy</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">Positive: Professionalism &amp; Quality
+                     "Very professional, friendly, explained the job process, eaisy to talk to, will call out for further jobs in the future."<br /><br /> - <i>Sheryll Lemon</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">Positive: Professionalism, Punctuality, Quality, Responsiveness &amp; Value<br /><br />
+                        "Exceptional service and very professional. Highly recommend."<br /><br /> - <i>T Roberts</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">Positive: Professionalism, Punctuality, Quality, Responsiveness &amp; Value<br /><br />
+                        "Cam provides an exceptional service and he is very professional. I highly recommend. ;-D"<br /><br /> - <i>Dee Lansdown</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">Positive: Professionalism, Punctuality, Quality, Responsiveness &amp; Value<br /><br />
+                        "Cam saved the day!! He came to the rescue in a very short notice after 4pm to check a gas leak at the house I just moved in.
+                         He fixed the issue and showed me all that I should/need to know. Thank you so so much for the great service!
+                          I highly recommend Flash Gas and Plumbing services."<br /><br /> - <i>Erica Guido</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">"Campbell replaced the Hot Water Cylinder in my rental property.
+                    He was efficient and communicated well with me by ringing me when the job was completed and took away the old cylinder for me and sent me photos of the new one to me.
+                       Very pleased with the job, Thank you."<br /><br /> - <i>Annette Clifford</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">Positive: Professionalism, Punctuality, Quality, Responsiveness &amp; Value<br /><br />
+                    "Cam installed gas hot water for us - what a pleasure working with someone who communicates well and on a regular basis! He turned up when he said he was and completed the
+                     work efficiently and very tidily as well!! He was very responsive to additional jobs being thrown in at the last minute.
+                      Have already recommended him to others!"<br /><br /> - <i>Shayne &amp; Jillian Ryder</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">Positive: Professionalism, Punctuality, Quality, Responsiveness &amp; Value<br /><br />
+                    "Amazing communication Does Perfect job very Happy with Work done will use again for any plumbing work."<br /><br /> - <i>Nathan Makris</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">"Cam shows up when he says he does with excellent workmanship.  He installed a new gas califont, fitted a new vanity and a toilet all at the price quoted.
+                     Would 100% use him again."<br /><br /> - <i>Nicholas Cutfield</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">"Cam replaced an old leaking hot water cylinder at my place in January 2021. He was punctual, efficient and conscientious.
+                     I would not hesitate to recommend him to anyone looking for a professional trades-person."<br /><br /> - <i>TeAute ICT</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">"Needed a new hot water cylinder in a hurry and Campbell was here in a flash and installed a new one we were very happy with his service
+                     and also impressed with the after job contact."<br /><br /> - <i>Bob Strong</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">"Cam the company owner traveled to complete our filter and water softener systm. This was all completed in one trip, very efficient.
+                    We were especially impressed how tidy the all the pipe work is :) Cam also gave us all some training on how it all works.
+                     Couldn't recommend "Flash" more for this type of whole house filter system. Thank you very much."<br /><br /> - <i>Kiel Mans</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">"Cam changed us over from low pressure hot water to an out door mains pressure water system.
+                    The whole process was made easy with great communication from Cam discussing our options for the space we had and working out our best option.
+                    High quality workmanship and completed on time with great service.<br /><br /> Would recommend to anyone and will use again in the future!"<br /><br /> - <i>Owen Ashton</i></div>
+                    <div className="divider"></div>
+                    <div className="testText">"Had our old hot water cylinder die on us recently. Flash gas and plumbing came out,
+                    installed new cylinder but unfortunately the pipes under the house were not up to the task.
+                    So an infinity gas water system was installed. Wow what a difference, pressure is out of this world.
+                       Big ups to Cam, was a nightmare job but he was equal to the task.<br /><br /> Highly recommend to anyone."<br /><br /> - <i>Andrew Gunn</i></div>
                     <div className="divider"></div>
                     <div className="testText">"Cam provided an efficient and highly professional service. We’re delighted to finally have constant hot running water for the whole family.
-                       Very neat job. Thanks Cam!"<br/><br/> - <i>Hugo Buckley</i></div>
+                       Very neat job. Thanks Cam!"<br /><br /> - <i>Hugo Buckley</i></div>
                     <div className="divider"></div>
                     <div className="testText">"Super quick to fit us in and did a great job, really happy to not hear a dripping shower every night now! Will definitely be recommending
-                       to anybody that needs a plumber."<br/><br/> - <i>Jordan Bottriell</i></div>
+                       to anybody that needs a plumber."<br /><br /> - <i>Jordan Bottriell</i></div>
                     <div className="divider"></div>
                     <div className="testText">"Cam worked with best care and respect during -what surprisingly turned into- an extremely tricky job! He managed the complexity of a total
-                       revamp to the hot water supply and installing an Infinity Gas Hot Water completing this work around the tenant. He left his work spaces very 
-                       tidily, kept in contact with me throughout, sending me pictures of progress and finish so as an absentee owner I have a good picture of the 
-                       new installation.<br /><br /> The tenant is delighted with new pressure and heat of the water. Given this turned into a major overhaul instead of hot 
-                       water cylinder replacement, Cam kept me posted with the change of pricing, helped out to minimise costs and updated me throughout. Highly 
-                       recommend to all! - Big thanks, Cam!"<br/><br/> - <i>Katja Widder</i></div>
+                    revamp to the hot water supply and installing an Infinity Gas Hot Water completing this work around the tenant. He left his work spaces very
+                    tidily, kept in contact with me throughout, sending me pictures of progress and finish so as an absentee owner I have a good picture of the
+                       new installation.<br /><br /> The tenant is delighted with new pressure and heat of the water. Given this turned into a major overhaul instead of hot
+                       water cylinder replacement, Cam kept me posted with the change of pricing, helped out to minimise costs and updated me throughout. Highly
+                       recommend to all! - Big thanks, Cam!"<br /><br /> - <i>Katja Widder</i></div>
                     <div className="divider"></div>
                     <div className="testText bottomText">"After a week of no hot water and unable to get a plumber to come or any callbacks, Cam was able to fit me in the day I called and had a
-                       quote for me within 24 hours. Job completed today first thing after the weekend. Thanks for the priority being a hot water emergency, so looking forward to a hot
-                       shower tonight.<br /><br /> Helpful, good communication, fair and personable. I am happy to recommend."<br/><br/> - <i>Cheryl White</i></div>        
+                    quote for me within 24 hours. Job completed today first thing after the weekend. Thanks for the priority being a hot water emergency, so looking forward to a hot
+                       shower tonight.<br /><br /> Helpful, good communication, fair and personable. I am happy to recommend."<br /><br /> - <i>Cheryl White</i></div>
                 </div>
-                    <Footer />
+                <Footer />
 
             </React.Fragment>
         )
